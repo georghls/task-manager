8546b55590e63182e5b547044433a7a4fc2e4a8a
georghls <hls.georgesa@gmail.com>
2026-07-27 12:23:45 -0300

    Fase 1: Inicializando o front

---
+++ task-manager-frontend/src/app/features/tasks/components/task-dashboard/task-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-dashboard',
+  imports: [],
+  templateUrl: './task-dashboard.html',
+  styleUrl: './task-dashboard.css',
+})
+export class TaskDashboard {}
---
+++ task-manager-frontend/src/app/features/tasks/components/task-form-modal/task-form-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-modal',
+  imports: [],
+  templateUrl: './task-form-modal.html',
+  styleUrl: './task-form-modal.css',
+})
+export class TaskFormModal {}
